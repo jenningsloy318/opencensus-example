aa6831176934f9e11c508fe3bb1e48689f89a407
Jennings Liu <jennings.liu@sap.com>
2018-09-20 17:16:19 +0800

    add new view/metric/tag for prometheus exporter

--- opencensus.go
+++ opencensus.go
@@ -3,8 +3,9 @@ package main
 import (
 	"context"
 	"go.opencensus.io/stats/view"
-	//	"go.opencensus.io/stats"
+	"go.opencensus.io/stats"
 	"go.opencensus.io/exporter/jaeger"
+	"go.opencensus.io/tag"
 	"go.opencensus.io/exporter/prometheus"
 	"go.opencensus.io/plugin/ochttp"
 	"go.opencensus.io/trace"
@@ -62,7 +63,10 @@ func main() {
 		log.Fatalf("Failed to register http default server views for metrics: %v", err)
 	}
 
-	view.SetReportingPeriod(1 * time.Second)
+
+
+
+	
 
 	// create jaeger exporter
 
@@ -78,19 +82,21 @@ func main() {
 		log.Fatalf("Failed to create the Jaeger exporter: %v", err)
 	}
 
-	trace.RegisterExporter(jaegerExporter)
+	
 	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
-
+	trace.RegisterExporter(jaegerExporter)
 	// connect hana db
 	driverName, err := ocsql.Register("hdb", ocsql.WithAllTraceOptions())
 
 	if err != nil {
 		log.Fatalf("Failed to register the ocsql driver: %v", err)
 	}
+
+
 	db, err := sql.Open(driverName, "hdb://SYSTEM:Toor1234@10.130.192.30:30015")
 
 	if err != nil {
-		log.Fatalf("Failed to open the SQL database: %v", err)
+		log.Fatalf("Failed to open the HANA database: %v", err)
 	}
 
 	defer func() {
@@ -101,23 +107,59 @@ func main() {
 		<-time.After(waitTime)
 	}()
 
-	ctx, span := trace.StartSpan(context.Background(), "HANAdb")
+	ctx, span := trace.StartSpan(context.Background(), "hana")
 	defer span.End()
 
-	fCtx, fSpan := trace.StartSpan(ctx, "dummy")
+	fCtx, fSpan := trace.StartSpan(ctx, "get_value")
+	
 	row := db.QueryRowContext(fCtx, "select 1 from dummy")
+	
 	fSpan.End()
+
 	var number string
 
 	if err := row.Scan(&number); err != nil {
 		log.Fatalf("Failed to get data: %v", err)
 	}
+	
+	svcnamekey, err := tag.NewKey("myservice/name")
+	if err != nil {
+	 log.Fatal(err)
+	}
 
+	pectx, err := tag.New(ctx,
+    tag.Insert(svcnamekey, "myservice"),
+	)
+
+	 if err != nil {
+		log.Fatal(err)
+	 }
+
+
+
+	videoSize := stats.Int64("myservice/video_size", "processed video size", "MB")
+
+	
+	
+	svcview:= view.View{
+		Name: "my.org/video_size_distribution",
+		Description: "distribution of processed video size over time",
+		TagKeys: []tag.Key{svcnamekey},
+		Measure: videoSize,
+		Aggregation: view.LastValue(),
+	}
+
+	if err := view.Register(&svcview); err != nil {
+	log.Fatal(err)
+	}
+	stats.Record(pectx, videoSize.M(102478))
+	view.SetReportingPeriod(1 * time.Second)
+	
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", prometheusExporter)
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/result", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, number)
+		fmt.Fprintf(w, html)
 	})
 	mux.Handle("/debug/", http.StripPrefix("/debug", zpages.Handler))
 	h := &ochttp.Handler{Handler: mux}
